bb404f2704512899a849d3b5a346529b84efa74f
alihamza511 <alihamza6170045@gmail.com>
2024-12-12 00:46:28 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <button>Click Me</button>
+      <Button>Bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
